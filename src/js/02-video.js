cb843d1784f015f493648a7c7505c7be12764bc4
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-02-28 12:35:14 +0200

    added task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const timeProgress = localStorage.getItem('videoplayer-current-time');
+
+function saveTimeProgress(e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}
+
+if (timeProgress) {
+  player.setCurrentTime(timeProgress);
+}
+
+player.on('timeupdate', throttle(saveTimeProgress, 1000));
