0e95f0c97172f91d7728b5385813770233e7575c
snevinchaniy <85674052+snevinchaniy@users.noreply.github.com>
2023-03-03 14:49:14 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,26 +5,32 @@ const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
-
-populateForm();
+// const formData = {};
 
 form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
-function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
+populateForm();
+
+// Отримання даних та збереження в localStorage
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  console.log(formData);
 }
 
+// Вивід даних в консоль та їх видалення з localStorage після відправки
 function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
   localStorage.removeItem(STORAGE_KEY);
-  console.log(formData);
 }
 
+// Перевірка даних в localStorage і запис в форму якщо вони є
 function populateForm() {
   const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
